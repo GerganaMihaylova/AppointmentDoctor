da86ab0c507be784756a2348f45775a2a84af136
Гергана Михайлова <gm04346881@edu.mon.bg>
2021-11-23 15:26:52 +0200

    23.11

---
+++ 4ex/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using _Appointment.Doctor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4ex.Controllers
+{
+    public class AccountController:Controller
+    {
+        private readonly ApplicationDbContext _db;
+            public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
